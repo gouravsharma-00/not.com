1ee3ca49b1eefe3cd6f3bd2e2f722ea875e35e33
gouravsharma-00 <gourav.sharma.glamat2932@gmail.com>
2024-10-18 18:27:50 +0530

    initial-phaze

---
+++ src/database/mongo.js
@@ -0,0 +1,32 @@
+// JavaScript: mongoDB 🍀
+import { MongoClient } from 'mongodb';
+
+const URI = process.env.URI;
+
+const client = new MongoClient(URI);
+const main = async () => {
+    try {
+        await client.connect();
+        console.log('Connected to mongoDB');
+
+        // Fetching Data
+        try {
+            const database = client.db('Employee');
+            const collection = database.collection('Dev');
+
+            const query = { name: 'Gourav' };
+            const result = await collection.findOne(query);
+            console.log(`Employee : ${result}`);
+        }catch (e) {
+            console.log('Query Failed: ', e);
+        }
+
+    }catch (e) {
+        console.log('Connection Failed: ', e);
+    }finally {
+        await client.close();
+        console.log('Connection Closed');
+    }
+}
+
+export default main;
